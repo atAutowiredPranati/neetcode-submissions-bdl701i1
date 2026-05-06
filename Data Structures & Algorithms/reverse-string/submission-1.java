cc13ba20572df2e982abf24fd9e657cc7ff68d38
Pranati Pastaria <143206157+atAutowiredPranati@users.noreply.github.com>
2026-05-06 13:28:37 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        reverse(s , 0 , s.length -1);
+        
+    }
+
+    private void reverse(char[]s , int l , int r){
+        while (l < r){
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
