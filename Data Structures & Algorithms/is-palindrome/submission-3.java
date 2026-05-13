fb5a81c0b95168f082783c5a7ca788728e763840
Pranati Pastaria <143206157+atAutowiredPranati@users.noreply.github.com>
2026-05-13 23:33:38 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder str = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                str.append(Character.toLowerCase(c));
+            }
+        }
+
+        if(str.toString().equals(str.reverse().toString())){
+            return true;
+        }
+        else return false;
+        
+    }
+}
