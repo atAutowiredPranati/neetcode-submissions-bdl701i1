7ee5f1064a7c4b6e98f842a87e098e92c977f765
Pranati Pastaria <143206157+atAutowiredPranati@users.noreply.github.com>
2026-06-03 19:57:53 +0530

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+        for(int i = 1 , j = i - 1; i < nums.length ; i++, j++){
+            if (nums[i] == nums[j]){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
